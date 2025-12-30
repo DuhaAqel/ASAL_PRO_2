dfcf5a809a8d1581c747660090f564b983172db6
yafa <yafajarrar1@gmail.com>
2025-12-31 01:15:21 +0200

    16

--- jscript1.js
+++ jscript1.js
@@ -48,4 +48,9 @@ function showDialog({
   overlay.style.position = "fixed";
   overlay.style.top = 0;
   overlay.style.left = 0;
+ overlay.style.width = "100vw";
+  overlay.style.height = "100vh";
+  overlay.style.background = "rgba(0,0,0,0.18)";
+  overlay.style.display = "flex";
+  overlay.style.alignItems = "center";
 
